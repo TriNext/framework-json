a9c276b1d874c8072864c4f59a75398de186d268
MattisKunstmann <mattis.kunstmann@trinext.de>
2023-10-24 15:07:15 +0200

    feat(addded comments for javadoc)

--- src/main/java/app/Placeholder.java
+++ src/main/java/app/Placeholder.java
@@ -1,14 +1,18 @@
 package app;
 
 /**
+ * A placeholder class to achieve a successful Jenkins build
  * @author Dennis Woithe
  */
 
 public class Placeholder {
 
+    /**
+     * a Function  that always returns true
+     * @return always True
+     */
     public boolean alwaysTrue() {
         return true;
     }
-
 }
 
