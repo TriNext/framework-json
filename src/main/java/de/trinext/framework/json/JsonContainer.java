c70164cc625f3446c025d273e435c91796043896
Dennis Woithe <woithe@trinext.de>
2023-11-13 13:35:50 +0100

    doc(Try-Extractors)
    Signed-off-by: Dennis Woithe <woithe@trinext.de>

--- src/main/java/de/trinext/framework/json/JsonContainer.java
+++ src/main/java/de/trinext/framework/json/JsonContainer.java
@@ -5,7 +5,7 @@ import java.math.BigInteger;
 import java.util.*;
 import java.util.stream.Stream;
 
-import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * The classification for a {@link JsonElement} that contains other {@link JsonElement}s.
@@ -17,7 +17,7 @@ import org.jetbrains.annotations.Nullable;
 @SuppressWarnings({"unused", "WeakerAccess"})
 public abstract sealed class JsonContainer<V> extends JsonElement<V> permits JsonList, JsonMap {
 
-    JsonContainer(@Nullable V value) {
+    JsonContainer(@NotNull V value) {
         super(value);
     }
 
@@ -58,40 +58,49 @@ public abstract sealed class JsonContainer<V> extends JsonElement<V> permits Jso
         return tryGetPath(jsonPath).flatMap(e -> e.tryGetEnum(enumType));
     }
 
+    /** @return {@link Optional<Number>} whether the target of the passed path is a {@link JsonNumber}. */
     public final Optional<Number> tryGetPathAsNumber(String jsonPath) {
         return tryGetPath(jsonPath).flatMap(JsonElement::tryGetNumber);
     }
 
+    /** @return {@link Optional<String>} whether the target of the passed path is a {@link JsonString}. */
     public final Optional<String> tryGetPathAsString(String jsonPath) {
         return tryGetPath(jsonPath).flatMap(JsonElement::tryGetString);
     }
 
+    /** @return {@link Optional<BigInteger>} whether the target of the passed path is a {@link JsonInteger}. */
     public final Optional<BigInteger> tryGetPathAsBigInt(String jsonPath) {
         return tryGetPath(jsonPath).flatMap(JsonElement::tryGetBigInt);
     }
 
+    /** @return {@link Optional<BigDecimal>} whether the target of the passed path is a {@link JsonDecimal}. */
     public final Optional<BigDecimal> tryGetPathAsBigDec(String jsonPath) {
         return tryGetPath(jsonPath).flatMap(JsonElement::tryGetBigDec);
     }
 
+    /** @return {@link Optional<Boolean>} whether the target of the passed path is a {@link JsonBool}. */
     public final Optional<Boolean> tryGetPathAsBool(String jsonPath) {
         return tryGetPath(jsonPath).flatMap(JsonElement::tryGetBool);
     }
 
     // ==== CONTAINER ======================================================== //
 
+    /** @return {@link Optional<JsonMap>} whether the target of the passed path is a {@link JsonMap}. */
     public final <T> Optional<T> tryGetPathAsObj(String jsonPath, Class<? extends T> cls) {
         return tryGetPath(jsonPath).flatMap(e -> e.tryGetObj(cls));
     }
 
+    /** @return {@link Optional<JsonList>} whether the target of the passed path is a {@link JsonList}. */
     public final <T> Optional<Stream<T>> tryGetPathAsStreamOf(String jsonPath, Class<? extends T> elemCls) {
         return tryGetPath(jsonPath).flatMap(e -> e.tryGetStreamOf(elemCls));
     }
 
+    /** @return {@link Optional<JsonList>} whether the target of the passed path is a {@link JsonList}. */
     public final <T> Optional<List<T>> tryGetPathAsListOf(String jsonPath, Class<? extends T> elemCls) {
         return tryGetPath(jsonPath).flatMap(e -> e.tryGetListOf(elemCls));
     }
 
+    /** @return {@link Optional<JsonList>} whether the target of the passed path is a {@link JsonList}. */
     public final <T> Optional<Set<T>> tryGetPathAsSetOf(String jsonPath, Class<? extends T> elemCls) {
         return tryGetPath(jsonPath).flatMap(e -> e.tryGetSetOf(elemCls));
     }
