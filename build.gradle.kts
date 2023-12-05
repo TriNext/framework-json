72a7095c0597337fa3ffdfc5e381bc6c558b9cf0
Dennis Woithe <woithe@trinext.de>
2023-12-05 14:34:49 +0100

    new Version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@
 import org.owasp.dependencycheck.gradle.extension.DependencyCheckExtension
 
 group = "de.trinext"
-version = "1.0.0"
+version = "1.0.5"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
